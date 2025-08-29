0aaaa6b5fdc1be768a18916c89508aec567dc63a
luiza <161471348+luizaueno@users.noreply.github.com>
2025-08-29 17:06:05 -0300

    Subindo arquivos extraídos do zip para iniciar repositório

---
+++ aulas/microsserviços/observacoes/index.js
@@ -0,0 +1,45 @@
+const express = require ('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+const { v4: uuidv4 } = require('uuid');
+
+observacoesPorLembreteId = {};
+
+
+app.use(bodyParser.json());
+
+ //:id é um placeholder
+ //exemplo: /lembretes/123456/observacoes
+ 
+app.put('/lembretes/:id/observacoes', async(req, res) => {
+const idObs = uuidv4();
+const { texto } = req.body;
+ //req.params dá acesso à lista de parâmetros da URL
+ observacoesDoLembrete =
+ observacoesPorLembreteId[req.params.id] || [];
+ observacoesDoLembrete.push({ id: idObs, texto });
+ observacoesPorLembreteId[req.params.id] =
+    observacoesDoLembrete;
+ await axios.post('http://localhost:10000/eventos', {
+     tipo: "ObservacaoCriada",
+     dados: {
+     id: idObs, texto, lembreteId: req.params.id
+     }
+     })
+res.status(201).send(observacoesDoLembrete);
+ });
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+res.send(observacoesPorLembreteId[req.params.id] || []);
+});
+
+app.post("/eventos", (req, res) => {
+    console.log(req.body);
+     res.status(200).send({ msg: "ok" });
+    });
+    
+app.listen(5000, (() => {
+ console.log('Lembretes. Porta 5000');
+ }));
