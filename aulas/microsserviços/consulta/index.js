69bac3c3e106e5bd7cf764c3c1358d98962c9759
luiza <161471348+luizaueno@users.noreply.github.com>
2025-09-12 11:56:19 -0300

    aula 12.09

--- aulas/microsserviços/consulta/index.js
+++ aulas/microsserviços/consulta/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const axios = require("axios");
+
 
 app.use(express.json());
 baseConsulta = {};
@@ -15,7 +17,14 @@ const funcoes = {
         observacoes.push(observacao);
         baseConsulta[observacao.lembreteId]["observacoes"] =
             observacoes;
-     }
+     },
+     ObservacaoAtualizada: (observacao) => {
+        const observacoes =
+            baseConsulta[observacao.lembreteId]["observacoes"];
+        const indice = observacoes.findIndex((o) => o.id ===
+            observacao.id);
+        observacoes[indice] = observacao;
+    },        
  };  
 
 app.get("/lembretes", (req, res) => {
@@ -23,8 +32,29 @@ app.get("/lembretes", (req, res) => {
 });
 
 app.post("/eventos", (req, res) => {
-    try {funcoes[req.body.tipo](req.body.dados);} catch(err){}
+    try {
+        funcoes[req.body.tipo](req.body.dados);} 
+    catch(err){}
     res.status(200).send(baseConsulta);
 });
 
-app.listen(6000, () => console.log("Consultas. Porta 6000"));
+app.listen(6000, async () => {
+    console.log("Consultas. Porta 6000")
+    const resp = await
+    axios.get("http://localhost:10000/eventos");
+    // axios entrega os dados na propriedade data
+    resp.data.forEach((valor, indice, colecao) => {
+        try {
+        funcoes[vbalor.tipo](valor.dados);
+        } 
+        catch {(err) () 
+        }
+        });
+});
+
+
+
+    
+
+
+  
